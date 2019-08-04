842fdb9a7115315f3c5b634010726db5fdedba91
Gustavo Garcia <gustavo.ggdrn@gmail.com>
2019-08-03 22:36:25 -0300

    Correção de indentação

--- script.js
+++ script.js
@@ -11,22 +11,22 @@
         return false;
       }
       // Array contendo os múltiplos valores de 3,5 e ambos
-      let resultado = [];
+    let resultado = [];
       // Contador de acordo com os parametros que usuario determina
-      for(let i=1;i<=qtd;i++){
-        if(Calcular(i,5) && Calcular(i,3)){
-            resultado.push(" Jovens Gênios ");
-          }else{
-            if(Calcular(i,3)){
-              resultado.push(" Jovens ");
-            }
-            else if(Calcular(i,5)){
-              resultado.push(" Gênios ");
-            }else {
-              resultado.push(i);
-            }
+    for(let i=1;i<=qtd;i++){
+      if(Calcular(i,5) && Calcular(i,3)){
+        resultado.push(" Jovens Gênios ");
+        }else{
+          if(Calcular(i,3)){
+            resultado.push(" Jovens ");
+          }
+          else if(Calcular(i,5)){
+            resultado.push(" Gênios ");
+          }else {
+            resultado.push(i);
+          }
+        }
       }
-    }
-      // Passar o resultado para o index
-      document.getElementById('resultado').innerHTML = resultado;
-    }
+    // Passar o resultado para o index
+    document.getElementById('resultado').innerHTML = resultado;
+  }
